90a84164fc6f3ca3da53ce56b810a840961f1398
Michał Klekowski <kleko09@gmail.com>
2019-08-01 19:59:28 +0200

    Updates

---
+++ precache-manifest.4e10fcb4c1807646946536ebd578e87d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb32ff16fa19a0d3db29d58f7dbc73fc",
+    "url": "/wroclawjug-random-attendee-v2/index.html"
+  },
+  {
+    "revision": "5de7645b4878a91333b9",
+    "url": "/wroclawjug-random-attendee-v2/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wroclawjug-random-attendee-v2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5de7645b4878a91333b9",
+    "url": "/wroclawjug-random-attendee-v2/static/js/main.c5cc6257.chunk.js"
+  },
+  {
+    "revision": "4bf9933d882ed55aadc3",
+    "url": "/wroclawjug-random-attendee-v2/static/js/runtime~main.8f0868c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wroclawjug-random-attendee-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
